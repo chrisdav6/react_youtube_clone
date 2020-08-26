810755cace5809c617d12e0ea94468347a174287
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-26 12:45:18 -0400

    Finished SidebarRows

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import SidebarRow from './SidebarRow';
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 
 const SideBar = () => {
-  return <div className='SideBar'>Sidebar</div>;
+  return (
+    <div className='SideBar'>
+      <SidebarRow Icon={HomeIcon} title='Home' active={true} />
+      <SidebarRow Icon={WhatshotIcon} title='Trending' />
+      <SidebarRow Icon={SubscriptionsIcon} title='Subscriptions' />
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title='Library' />
+      <SidebarRow Icon={HistoryIcon} title='History' />
+      <SidebarRow Icon={OndemandVideoIcon} title='Your Videos' />
+      <SidebarRow Icon={WatchLaterIcon} title='Watch Later' />
+      <SidebarRow Icon={ThumbUpIcon} title='Liked Videos' />
+    </div>
+  );
 };
 
 export default SideBar;
---
+++ src/components/SidebarRow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SidebarRow = ({ Icon, title, active }) => {
+  return (
+    <div className={`SidebarRow ${active && 'active'}`}>
+      <span className='icon'>{<Icon />}</span> {title}
+    </div>
+  );
+};
+
+export default SidebarRow;
