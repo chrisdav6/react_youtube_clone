d5bd26c38c7cfca8ac5af9654da4fa37a53330c6
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-27 11:44:08 -0400

    Installed moment for timestamp, also building MovieCard structure

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Moment from 'react-moment';
 
 function MovieCard({ movie }) {
   return (
-    <div className='movie'>
-      <div className='movieImage'>
-        <img
-          src={movie.snippet.thumbnails.high.url}
-          alt={movie.snippet.title}
+    <div className='MovieCard'>
+      <img src={movie.snippet.thumbnails.high.url} alt={movie.snippet.title} />
+      <div className='MovieCard__info'>
+        <Avatar
+          className='MovieCard__avatar'
+          src={movie.snippet.thumbnails.default.url}
+          alt={movie.snippet.channelId}
         />
+        <div className='MovieCard__text'>
+          <h4>{movie.snippet.title}</h4>
+          <p>{movie.snippet.channelTitle}</p>
+          <p>
+            900K views : <Moment fromNow>{movie.snippet.publishTime}</Moment>
+          </p>
+        </div>
+        <a
+          className='link'
+          href={`https://www.youtube.com/watch?v=${movie.id.videoId}`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Link to Video
+        </a>
       </div>
-      <h3 className='movieTitle'>{movie.snippet.title}</h3>
-      <p className='desc'>{movie.snippet.description}</p>
-      <a
-        className='link'
-        href={`https://www.youtube.com/watch?v=${movie.id.videoId}`}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Link to Video
-      </a>
     </div>
   );
 }
