7dc864971aa204e8d9a11d68157e4e9b459b1382
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-31 09:10:02 -0400

    Styling search filter icon and text

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SearchCard from './SearchCard';
+import TuneIcon from '@material-ui/icons/Tune';
 
 const Search = ({ movies }) => {
   return (
     <div className='Search'>
-      {movies.map((movie, idx) => (
-        <SearchCard key={idx} movie={movie} />
-      ))}
+      <div className='Search__filter'>
+        <TuneIcon className='Search__icon' />{' '}
+        <span className='Search__filter__text'>FILTER</span>
+      </div>
+
+      <div className='Search__contents'>
+        {movies.map((movie, idx) => (
+          <SearchCard key={idx} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 };
